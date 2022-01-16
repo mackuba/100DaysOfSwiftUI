80f43bc1af3e4619aea64d7afde95f477b8dffbd
Kuba Suder <jakub.suder@gmail.com>
2022-01-16 14:40:15 +0200

    implemented converter screen

--- Converter/Converter/ContentView.swift
+++ Converter/Converter/ContentView.swift
@@ -7,10 +7,80 @@
 
 import SwiftUI
 
+enum LengthUnit: String, CaseIterable {
+    case millimeters
+    case centimeters
+    case meters
+    case kilometers
+    case inches
+    case feet
+    case yards
+    case miles
+
+    var conversionFactor: Double {
+        switch self {
+            case .millimeters: return 0.001
+            case .centimeters: return 0.01
+            case .meters: return 1
+            case .kilometers: return 1000
+            case .inches: return 0.0254
+            case .feet: return 0.3048
+            case .yards: return 0.9144
+            case .miles: return 1609.344
+        }
+    }
+
+}
+
 struct ContentView: View {
+    @State private var inputUnit: LengthUnit = .kilometers
+    @State private var targetUnit: LengthUnit = .miles
+    @State private var inputAmount: Double = 0.0
+    @FocusState private var amountHasFocus: Bool
+
+    var convertedAmount: Double {
+        return inputAmount * inputUnit.conversionFactor / targetUnit.conversionFactor
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {}
+
+                Section {
+                    Picker("From", selection: $inputUnit) {
+                        ForEach(LengthUnit.allCases, id: \.self) { unit in
+                            Text("\(unit.rawValue)")
+                        }
+                    }
+                    .pickerStyle(.automatic)
+
+                    Picker("To", selection: $targetUnit) {
+                        ForEach(LengthUnit.allCases, id: \.self) { unit in
+                            Text("\(unit.rawValue)")
+                        }
+                    }
+                    .pickerStyle(.automatic)
+                }
+
+                Section {
+                    TextField("Amount", value: $inputAmount, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountHasFocus)
+
+                    Text(convertedAmount, format: .number)
+                }
+            }
+            .navigationTitle("Convert value")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountHasFocus = false
+                    }
+                }
+            }
+        }
     }
 }
 
