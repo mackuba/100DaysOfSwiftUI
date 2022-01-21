8d5206a54408a8c1a9a184cf247ce7e190418a84
Kuba Suder <jakub.suder@gmail.com>
2022-01-21 17:22:19 +0200

    picking flags and resetting the view

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,9 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+
+    @State var correctAnswer = Int.random(in: 0...2)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+
+            VStack(spacing: 50) {
+                VStack(spacing: 5) {
+                    Text("Tap the flag of:")
+                    Text(countries[correctAnswer]).font(.title2)
+                }
+                .foregroundColor(.white)
+
+                VStack(spacing: 30) {
+                    ForEach(0...2, id: \.self) { i in
+                        Button {
+                            flagTapped(index: i)
+                        } label: {
+                            Image(countries[i])
+                        }
+                    }
+                }
+            }
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: pickAnotherFlag)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+
+    func flagTapped(index: Int) {
+        scoreTitle = (index == correctAnswer) ? "Correct!" : "Wrong"
+        showingScore = true
+    }
+
+    func pickAnotherFlag() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
