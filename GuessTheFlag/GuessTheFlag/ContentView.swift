7e91c7cf3884a6f9f6fa8e357aa90d00fe15e719
Kuba Suder <jakub.suder@gmail.com>
2022-01-21 17:54:19 +0200

    design tweaks

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -17,25 +17,37 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            Color.blue
-                .ignoresSafeArea()
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    Color.blue,
+                    Color(red: 0, green: 0, blue: 0.6)
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
 
             VStack(spacing: 50) {
                 VStack(spacing: 5) {
                     Text("Tap the flag of:")
-                    Text(countries[correctAnswer]).font(.title2)
+                    Text(countries[correctAnswer]).font(.title.weight(.semibold))
                 }
                 .foregroundColor(.white)
 
-                VStack(spacing: 30) {
+                VStack(spacing: 40) {
                     ForEach(0...2, id: \.self) { i in
                         Button {
                             flagTapped(index: i)
                         } label: {
                             Image(countries[i])
+                                .shadow(radius: 8)
                         }
                     }
                 }
+
+                Text("Score: ???")
+                    .foregroundColor(Color(red: 0.75, green: 0.75, blue: 1.0))
+                    .font(.callout)
             }
         }
         .alert(scoreTitle, isPresented: $showingScore) {
